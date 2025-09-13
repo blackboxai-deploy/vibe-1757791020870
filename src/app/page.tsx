c4ffe891dd179da7d40872a244abd9a031737bf7
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-14 00:47:08 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { WeekdayApp } from '@/components/WeekdayApp';
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen">
+      <WeekdayApp />
+    </div>
+  );
+}
